bd8e4b3e33ec8993034d0ec4e8749bada965fb67
Evan Keeton <keetonnj@edison.k12.nj.us>
2018-11-08 07:52:51 -0500

    Add built date to built site
    - footer.py - uses "date" command
    - Appended to footer of all built pages
        - Currently just "home", "2018"

--- Build-Assets/footer.py
+++ Build-Assets/footer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import os
+
+date_str = os.popen('date "+%d %b %Y"').read().strip()
+
 lines = [
 '    <div class="w3-container w3-black w3-center w3-opacity w3-padding-16">',
 '      <h1 class="w3-margin w3-xlarge">',
@@ -8,7 +12,8 @@ lines = [
 '      <p>Powered by',
 '        <a href="https://www.w3schools.com/w3css/w3css_templates.asp" target="_blank">',
 '          Start Page',
-'        </a>',
+'        </a>.<br />',
+'        Built on ' + date_str + '.',
 '      </p>',
 '    </div>',
 ]
